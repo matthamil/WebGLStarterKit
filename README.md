1f6334949ba93571ff763a134527c678c6dcf584
Matt Hamil <j.matthew.hamil@gmail.com>
2017-08-18 22:08:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# WebGL Playground
+
+[View the Playground](http://webglplayground.surge.sh/).
+
+Following along a WebGL tutorial by [Indigo Code](https://www.youtube.com/channel/UCufDmM2MGQlbfS3luzgVDxw). Adapted to use [Webpack](https://webpack.js.org) and [npm](https://www.npmjs.com/) modules.
+
+## Installation
+
+Download the repo locally to your machine. Run `yarn` to install dependencies.
+
+Use `yarn start` to start the Webpack development server.
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'WebGL Sandbox'
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
