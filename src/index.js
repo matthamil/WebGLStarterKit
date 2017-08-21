6be83af826b76c6a028595d91c18a5a51a211ff7
Matt Hamil <j.matthew.hamil@gmail.com>
2017-08-20 23:17:24 -0500

    Fixed bug from last change

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const init = () => {
     vertexShaderSrc: vertexShaderText,
     fragmentShaderSrc: fragmentShaderText
   };
-  const gl = initWebGLProgram(canvas)(shaderConfig);
+  const { gl } = initWebGLProgram(canvas)(shaderConfig);
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
