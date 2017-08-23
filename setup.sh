1cf1dc53e15b30ec201371e167e9f2e8ee9a2e99
Matt Hamil <j.matthew.hamil@gmail.com>
2017-08-23 01:13:41 -0500

    initial setup for npm

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 yarn
-rm .travis.yml # remove Travis CI config
 printf "# WebGL Project\nThis project was scaffolded using the [WebGL Starter Kit](https://github.com/matthamil/WebGLStarterKit)." > README.md
 sed -i '' /'setup'/d package.json
 rm setup.sh # self-destroy this setup script
